43a8248a65a195d6593e9f1100c60a382f7a3691
PaolaArismendy <paolaarismendy431@gmail.com>
2025-09-24 16:47:57 -0500

    Tercera entrega

--- MARIAPAULINAFORONDA - 1020433058/App.js
+++ MARIAPAULINAFORONDA - 1020433058/App.js
@@ -25,3 +25,27 @@ recibirDano(30);
 ganarExperiencia(48);
 mostrarEstado();
  
+// Tercera entrega
+
+const entrenar = (dias) => {
+  for (let i = 0; i < dias; i++) {
+    ganarExperiencia(5);
+  }
+  console.log("Entrenamiento completado.");
+};
+
+function verificarNivel() {
+  if (experienciaHeroe > 100) {
+    console.log("¡Felicidades! Has alcanzado el nivel de 'Maestro'.");
+    if (vidaHeroe < 50) {
+      console.log("Pero te encuentras débil. Sería prudente descansar.");
+    }
+  } else if (experienciaHeroe > 30) {
+    console.log("Nivel alcanzado: 'Aventurero'. ¡Sigue así!");
+  } else {
+    console.log("Nivel actual: 'Novato'.");
+  }
+}
+
+entrenar(10);
+verificarNivel();
