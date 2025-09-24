cad04f00b5e70e6f5673a5c54ac07b294fc4c838
SEBASTIAN HERRERA <ecoshop863@gmail.com>
2025-09-24 16:02:54 -0500

    Fase 1: Configuración inicial del producto y su inventario.

---
+++ 1116130856-Juan-Sebastian-Herrera-Ochoa/app.js
@@ -0,0 +1,17 @@
+
+// Variables solicitadas
+let nombreProducto = "Laptop Gamer"
+let stockProducto = 10
+let precioProducto = 1500
+
+// Función para mostrar el inventario
+function mostrarInventario() {
+    console.log("Producto: " + nombreProducto)
+    console.log("Stock: " + stockProducto)
+    console.log("Precio: $" + precioProducto)
+}
+
+// Llamada a la función para mostrar el inventario
+mostrarInventario()
+
+
