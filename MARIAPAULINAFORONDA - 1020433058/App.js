1780ec661f413bc9417da666f6335a9136082fb0
PaolaArismendy <paolaarismendy431@gmail.com>
2025-09-24 16:44:55 -0500

    Primera entrega

---
+++ MARIAPAULINAFORONDA - 1020433058/App.js
@@ -0,0 +1,11 @@
+// Primera entrega
+let nombreHeroe = "M.stark";
+let vidaHeroe = 100;
+let experienciaHeroe = 0;
+
+function mostrarEstado() {
+  console.log(`Héroe: ${nombreHeroe} | Vida: ${vidaHeroe} HP | Experiencia: ${experienciaHeroe} XP`);
+}
+
+mostrarEstado();
+
