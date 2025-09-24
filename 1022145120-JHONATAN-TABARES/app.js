50bed7a012147e192f3555401da4bf15b452e560
JhonTabar <tabaresjhonatan771@gmail.com>
2025-09-24 15:33:52 -0500

    Fase 1: Configuración inicial del producto y su inventario

---
+++ 1022145120-JHONATAN-TABARES/app.js
@@ -0,0 +1,9 @@
+let nombreProducto = "LapTopGamer"
+let stockProducto = 10
+let precioProducto = 1500
+
+function mostrarInventario(){
+console.log("Producto: ", nombreProducto, " | Stock : ", stockProducto," unidades | Precio: $", precioProducto)
+}
+
+mostrarInventario();
