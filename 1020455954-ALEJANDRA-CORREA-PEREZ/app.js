92b28373996254081b2e265b7aa8674558c6a4bb
alejacorrea <alejandracp63@gmail.com>
2025-09-24 14:57:31 -0500

    Fase 1: Configuración inicial del producto y su inventario

---
+++ 1020455954-ALEJANDRA-CORREA-PEREZ/app.js
@@ -0,0 +1,9 @@
+let nombreProducto = "Laptop Gamer";
+let stockProducto = 10;
+let precioProducto = 1500;
+
+function mostrarInventario() {
+    console.log(`Producto: ${nombreProducto} | ` + `Stock: ${stockProducto} | ` + `Precio: $${precioProducto}`);
+}
+
+mostrarInventario();
