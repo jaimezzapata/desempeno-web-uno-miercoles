e86aaf9b62b1be4ac26749b797943b36e8b41ddb
daniellema11 <dani_lema_92@hotmail.com>
2025-09-24 14:57:49 -0500

    Fase 1: Configuración inicial y estado de la mascota completado

---
+++ 1152694074-DanielEstebanLemaGamboa/app.js
@@ -0,0 +1,13 @@
+let nombreMascota = prompt("Ingrese el nombre de su mascota");
+let hambre = 50
+let felicidad = 50
+let energia = 50
+
+function mostrarEstado() {
+    console.log("Estado actual de la mascota:");
+    console.log("hambre: " + hambre);
+    console.log("felicidad: " + felicidad);
+    console.log("energia: " + energia);
+}
+
+mostrarEstado();
