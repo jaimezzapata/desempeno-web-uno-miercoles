2d0bbf37418225b2db882d285a4452a69e9a3235
daniellema11 <dani_lema_92@hotmail.com>
2025-09-24 15:41:21 -0500

    Fase 2: Implementadas funciones para alimentar y jugar

--- 1152694074-DanielEstebanLemaGamboa/app.js
+++ 1152694074-DanielEstebanLemaGamboa/app.js
@@ -4,10 +4,35 @@ let felicidad = 50
 let energia = 50
 
 function mostrarEstado() {
-    console.log("Estado actual de la mascota:");
+    console.log("Estado actual de la mascota:" + nombreMascota);
     console.log("hambre: " + hambre);
     console.log("felicidad: " + felicidad);
     console.log("energia: " + energia);
 }
+
+const alimentar = function (puntosHambre) {
+    hambre += puntosHambre;
+    if (hambre > 100) {
+        hambre = 100;
+    }
+    console.log("¡Qué rico! Hambre ahora en: " + hambre);
+}
+alimentar(30);
+
+const jugar = (puntosFelicidad) => {
+    felicidad += puntosFelicidad;
+    energia -= 10;
+
+    if (felicidad > 100) {
+        felicidad = 100;
+    }
+
+    if (energia < 0) {
+        energia = 0;
+    }
+
+    console.log("¡A jugar! Felicidad: " + felicidad + "," + "energia: " + energia);
+}
+jugar(25);
 
 mostrarEstado();
