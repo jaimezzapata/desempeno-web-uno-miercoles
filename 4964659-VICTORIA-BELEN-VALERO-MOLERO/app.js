069670790df16cb96d6d23273830b0c78b4109d0
VictoriaBel788 <valerovictoria299@gmail.com>
2025-09-26 11:29:36 -0500

    Fase 2: Implementadas funciones para alimentar y jugar

--- 4964659-VICTORIA-BELEN-VALERO-MOLERO/app.js
+++ 4964659-VICTORIA-BELEN-VALERO-MOLERO/app.js
@@ -12,3 +12,33 @@ function mostrarEstado() {
 }
 
 mostrarEstado();
+
+const alimentar = function (puntosHambre) {
+    hambre += puntosHambre;
+
+    if (hambre > 100) {
+        hambre = 100;
+    }
+    console.log("¡Qué rico! Hambre ahora en: " + hambre);
+}
+
+alimentar(30);
+mostrarEstado();
+
+const jugar = (puntosFelicidad) => {
+    felicidad += puntosFelicidad;
+    energia -= 10;
+
+    if (felicidad > 100) {
+        felicidad = 100;
+    } else if (energia < 0) {
+        energia = 0;
+    }
+
+    console.log("¡A jugar! Felicidad: " + felicidad + ", Energía: " + energia);
+}
+
+
+jugar(25);
+mostrarEstado();
+
