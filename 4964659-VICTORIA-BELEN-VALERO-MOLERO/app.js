6de2824261603753a5860ff19d5ab8c4529bb235
VictoriaBel788 <valerovictoria299@gmail.com>
2025-09-26 11:53:29 -0500

    Fase 3: Añadida simulación del tiempo y lógica de estado

--- 4964659-VICTORIA-BELEN-VALERO-MOLERO/app.js
+++ 4964659-VICTORIA-BELEN-VALERO-MOLERO/app.js
@@ -22,8 +22,8 @@ const alimentar = function (puntosHambre) {
     console.log("¡Qué rico! Hambre ahora en: " + hambre);
 }
 
-alimentar(30);
-mostrarEstado();
+// alimentar(30);
+// mostrarEstado();
 
 const jugar = (puntosFelicidad) => {
     felicidad += puntosFelicidad;
@@ -39,6 +39,66 @@ const jugar = (puntosFelicidad) => {
 }
 
 
-jugar(25);
-mostrarEstado();
+// jugar(25);
+// mostrarEstado();
 
+const simularPasoDelTiempo = (horas) => {
+    for (let i = 1; i <= horas; i++) {
+        
+        hambre -= 5;
+        felicidad -= 3;
+        energia -= 2;
+
+        if (hambre < 0) hambre = 0;
+        if (felicidad < 0) felicidad = 0;
+        if (energia < 0) energia = 0;
+
+    }
+    console.log("Han pasado " + horas + " horas. Estado actualizado:");
+    mostrarEstado();
+}; 
+
+
+function revisarEstado() {
+    if (hambre < 20) {
+        console.log("¡Tengo mucha hambre!");
+        
+        if (energia < 15) {
+            console.log("Y estoy muy cansado...");
+        }
+    }
+}
+
+
+
+function evaluarFelicidad() {
+    let nivel; 
+
+    if (felicidad > 80) {
+        nivel = 1;
+    } else if (felicidad >= 50 && felicidad <= 80) {
+        nivel = 2;
+    } else if (felicidad >= 20 && felicidad < 50) {
+        nivel = 3;
+    } else {
+        nivel = 0; 
+    }
+
+    switch (nivel) {
+        case 1:
+            console.log("¡Estoy muy feliz! :D");
+            break;
+        case 2:
+            console.log("Me siento bien, pero podríamos jugar.");
+            break;
+        case 3:
+            console.log("Me siento un poco triste...");
+            break;
+        default:
+            console.log("Necesito atención.");
+    }
+}
+
+simularPasoDelTiempo(5);
+revisarEstado();
+evaluarFelicidad();
