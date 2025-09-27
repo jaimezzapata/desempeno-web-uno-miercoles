0530baf252b027367113df749b7ecdec32d0b212
dianabello <leidyaparicio2703@gmail.com>
2025-09-26 20:45:42 -0500

    Fase 2: Implementadas funciones de daño y ganancia de experiencia

--- CC-1101448495-Leidys-Diana-Aparicio-Bello/app.js
+++ CC-1101448495-Leidys-Diana-Aparicio-Bello/app.js
@@ -7,3 +7,18 @@ function mostrarEstado() {
 }
 
 mostrarEstado();
+
+function recibirDano(cantidadDano) {
+    vidaHeroe -= cantidadDano;
+    if (vidaHeroe < 0) vidaHeroe = 0;
+    console.log(`El héroe recibió ${cantidadDano} de daño. Vida restante: ${vidaHeroe}`);
+}
+
+const ganarExperiencia = (cantidadExperiencia) => {
+    experienciaHeroe += cantidadExperiencia;
+    console.log(`¡El héroe ganó ${cantidadExperiencia} de experiencia! Total: ${experienciaHeroe}`);
+}
+
+recibirDano(25);
+ganarExperiencia(50);
+mostrarEstado();
