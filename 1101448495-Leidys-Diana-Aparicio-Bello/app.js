b74e5eb355c7e6dde5b259309b604b22fa6a2d31
dianabello <leidyaparicio2703@gmail.com>
2025-09-26 20:26:28 -0500

    Fase 1: Creación del héroe y su estado inicial

---
+++ 1101448495-Leidys-Diana-Aparicio-Bello/app.js
@@ -0,0 +1,9 @@
+let nombreHeroe = "mujer maravilla";
+let vidaHeroe = 100;
+let experienciaHeroe = 0;
+
+function mostrarEstado() {
+    console.log(`Héroe: ${nombreHeroe} | Vida: ${vidaHeroe} HP | Experiencia: ${experienciaHeroe} XP`);
+}
+
+mostrarEstado();
