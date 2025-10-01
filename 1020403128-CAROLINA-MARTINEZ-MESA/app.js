2572276f5a07dcf8e0ef27a98916ff9a0d6a479d
caromm80 <caromm80@gmail.com>
2025-10-01 10:33:33 -0500

    Fase 1: Configuración inicial del producto y su inventario

---
+++ 1020403128-CAROLINA-MARTINEZ-MESA/app.js
@@ -0,0 +1,12 @@
+alert("Mmomento 2 Carolina Martínez Mesa");
+
+let nombreProducto = "Monstera deliciosa variegada";
+let stockProducto = 10;
+let precioProducto = 1500;
+
+function mostrarInventario() {
+    console.log("Producto: " + nombreProducto + ", Stock: " + stockProducto + ", Precio: $" + precioProducto);
+    
+    
+}
+mostrarInventario();
