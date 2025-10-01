e46de55cd4ac49d4da30a2a68bddec09b71f6522
caromm80 <caromm80@gmail.com>
2025-10-01 11:12:44 -0500

    Fase 3: Añadida simulación de ventas y alertas de stock

--- 1020403128-CAROLINA-MARTINEZ-MESA/app.js
+++ 1020403128-CAROLINA-MARTINEZ-MESA/app.js
@@ -26,6 +26,30 @@ const reponerStock = (cantidadRepuesta) => {
     console.log("Stock repuesto. Se añadieron " + cantidadRepuesta + " unidades de " +nombreProducto + " Stock total: "+stockProducto);
 }
 
+
 registrarVenta(3);
 reponerStock(5);
 mostrarInventario();
+
+const simularVentas = (numeroDeVentas) => {
+    for (let i = 0; i < numeroDeVentas; i++) {
+        registrarVenta(1);
+    }
+}
+
+function revisarStock() {
+    if (stockProducto === 0) {
+        console.log("ALERTA CRÍTICA: Producto agotado.");
+    } else if (stockProducto < 5 && stockProducto > 0) {
+        console.log("Advertencia: Stock bajo. Quedan solo " +stockProducto +" unidades. ");
+        if (precioProducto > 1000) {
+            console.log("Este es un producto de alto valor. Sugerir reposición inmediata.");
+        }
+    } else {
+        console.log("El stock del producto se encuentra en un nivel saludable.");
+    }
+}
+
+
+simularVentas(7);
+revisarStock();
