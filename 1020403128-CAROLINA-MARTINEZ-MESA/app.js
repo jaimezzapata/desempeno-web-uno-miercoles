bdac41466ec38d257daa73ccaffa309f6d330a10
caromm80 <caromm80@gmail.com>
2025-10-01 11:02:22 -0500

    Fase 2: Implementadas funciones para registrar ventas y reponer stock

--- 1020403128-CAROLINA-MARTINEZ-MESA/app.js
+++ 1020403128-CAROLINA-MARTINEZ-MESA/app.js
@@ -1,12 +1,31 @@
-alert("Mmomento 2 Carolina Martínez Mesa");
-
 let nombreProducto = "Monstera deliciosa variegada";
 let stockProducto = 10;
 let precioProducto = 1500;
 
 function mostrarInventario() {
-    console.log("Producto: " + nombreProducto + ", Stock: " + stockProducto + ", Precio: $" + precioProducto);
+    console.log("Producto: " + nombreProducto + ", Stock: " + stockProducto + ", Precio: $ " + precioProducto);
     
     
 }
+mostrarInventario();
+
+let registrarVenta = function( cantidadVendida) {
+    if (cantidadVendida <= stockProducto) {
+        stockProducto -= cantidadVendida;
+        console.log("Venta exitosa. Se vendieron: "+cantidadVendida + " unidades de: " + nombreProducto +"  Stock restante: " +stockProducto);
+    } else {
+        console.log(" No hay suficiente stock para completar la venta.");
+    }
+
+    
+}
+
+
+const reponerStock = (cantidadRepuesta) => {
+    stockProducto += cantidadRepuesta;
+    console.log("Stock repuesto. Se añadieron " + cantidadRepuesta + " unidades de " +nombreProducto + " Stock total: "+stockProducto);
+}
+
+registrarVenta(3);
+reponerStock(5);
 mostrarInventario();
