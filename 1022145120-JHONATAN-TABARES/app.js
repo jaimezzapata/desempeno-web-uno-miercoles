ac9ab26a65dba1450d4fb164342a72817ef4eb48
Jhonatan Tabares <tabaresjhonatan771@gmail.com>
2025-09-30 22:45:15 -0500

    Fase 2: Implementadas funciones para registrar ventas y reponer stock

--- 1022145120-JHONATAN-TABARES/app.js
+++ 1022145120-JHONATAN-TABARES/app.js
@@ -6,4 +6,22 @@ function mostrarInventario(){
 console.log("Producto: ", nombreProducto, " | Stock : ", stockProducto," unidades | Precio: $", precioProducto)
 }
 
+let registrarVenta = function (cantidadVenta){
+    if(cantidadVenta <= stockProducto){
+        cantidadVenta - stockProducto
+        console.log("Venta exitosa. Se añadieron " + cantidadVenta + " unidades de " + nombreProducto + ". Stock total: " + stockProducto);
+    } 
+};
+
+let reponerStock = (cantidadRepuesta) => {
+stockProducto += cantidadRepuesta;
+console.log("Stock repuesto. Se añadieron " + cantidadRepuesta + ". Stock total: " + stockProducto)
+};
+
+registrarVenta(3);
+
+reponerStock(5);
+
 mostrarInventario();
+
+
