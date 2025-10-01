f155136bfcde7647a6e5c8e295d31e189018b63c
Jhonatan Tabares <tabaresjhonatan771@gmail.com>
2025-10-01 01:44:21 -0500

    Fase 3: Añadida simulación de ventas y alertas de stock

--- 1022145120-JHONATAN-TABARES/app.js
+++ 1022145120-JHONATAN-TABARES/app.js
@@ -18,6 +18,33 @@ stockProducto += cantidadRepuesta;
 console.log("Stock repuesto. Se añadieron " + cantidadRepuesta + ". Stock total: " + stockProducto)
 };
 
+let simularVentas = (numeroDeVentas) => {
+    for (let i = 0; i < numeroDeVentas; i++ ){
+        registrarVenta(1);
+    }
+};
+
+function revisarStock(){
+    if (stockProducto == 0){
+        console.log("ALERTA CRÍTICA: Producto agotado.")
+        alert("ALERTA CRÍTICA: Producto agotado.")
+    } else if(stockProducto >0 && stockProducto < 5) {
+        console.log("Advertencia: Stock bajo. Quedan solo " + stockProducto + " unidades.")
+        alert("Advertencia: Stock bajo. Quedan solo " + stockProducto + " unidades.")
+    }
+    else if (precioProducto > 1000){
+        console.log("Este es un producto de alto valor. Sugerir reposición inmediata.")
+        alert("Este es un producto de alto valor. Sugerir reposición inmediata.")
+    } else{
+        console.log("El stock del producto se encuentra en un nivel saludable.")
+        alert("El stock del producto se encuentra en un nivel saludable.")
+    }
+};
+
+simularVentas(7);
+
+revisarStock();
+
 registrarVenta(3);
 
 reponerStock(5);
